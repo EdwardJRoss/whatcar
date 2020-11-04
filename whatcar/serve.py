b61be5143067e13d518ace0f6f554e019e831e54
Edward Ross <edward@skeptric.com>
2020-11-04 16:59:15 +1100

    Add models to repository
    It's not ideal having a 90MB file in the repository, but it's easier
    to store it with the code and not have to deal with the idiosyncracies
    of LFS.

--- whatcar/serve.py
+++ whatcar/serve.py
@@ -17,7 +17,6 @@ from pathlib import Path
 ################################################################################
 
 def get_learner():
-    download_learner()
     with open('class_names.csv') as f:
         # Skip header
         f.readline()
@@ -29,13 +28,6 @@ def get_learner():
     learn.load('makemodel-392')
     return learn
 
-
-URL = 'https://storage.googleapis.com/whatcar-model/makemodel-392.pth'
-def download_learner(force=False):
-    path = Path('models/makemodel-392.pth')
-    if force or not path.exists():
-        path.parent.mkdir(exist_ok=True)
-        urlretrieve(URL, path)
 
 LEARN = get_learner()
 
